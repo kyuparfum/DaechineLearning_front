f7c062c884faa8ebcaea278dab33dd11489eec5c
HYnllvo <gpalsu0812@naver.com>
2023-05-25 16:29:16 +0900

    x-csrf-token 에러

--- static/js/api.js
+++ static/js/api.js
@@ -1,52 +1,99 @@
 window.onload = () => {
-    console.log("스크립트 로딩 확인!")
+    console.log("회원가입, 로그인 api")
+}
+
+// csrfToken 가져오기
+function getCookie(name) {
+    if (!document.cookie) {
+        return null;
+    }
+
+    const xsrfCookies = document.cookie.split(';')
+        .map(c => c.trim())
+        .filter(c => c.startsWith(name + '='));
+
+    if (xsrfCookies.length === 0) {
+        return null;
+    }
+    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+}
+const csrfToken = getCookie('csrftoken');
+if (csrfToken != '') {
+    console.log('csrf값은:' + csrfToken);
 }
 
 // 회원가입
-async function handleSignin() {    // handle=눌렀을때 실행(처리)한다는 의미
-    const email = document.getElementById("email").value      // 변하지 않는 변수는 const/ var,let은 변하는 변수
+async function handleSignin() {
+    const email = document.getElementById("email").value
     const username = document.getElementById("username").value
-    const password = document.getElementById("password").value
-    console.log(email, username, password)
+    const password1 = document.getElementById("password1").value
+    const password2 = document.getElementById("password2").value
+    console.log(email, username, password1, password2)
 
+    let formData = new FormData();
+    formData.append(email, email)
+    formData.append(username, username)
+    formData.append(password1, password1)
+    formData.append(password2, password2)
+    console.log(formData)
 
     const response = await fetch(`${back_base_url}/users/signup/`, {
         headers: {
-            'content-type': 'application/json',
+
+            'content-type': 'multipart/form-data',
+            'X-CSRF-TOKEN': csrfToken,
+
         },
+        // xhr.setRequestHeader('X-CSRF-Token', csrf_cookieValue);
         method: 'POST',
-        body: JSON.stringify({
-            "email": email,
-            "username": username,
-            "password": password
-        })
+        // body: JSON.stringify({
+        //     "email": email,
+        //     "username": username,
+        //     "password1": password1,
+        //     "password2": password2
+        // })
+        body: formData
     })
 
     if (response.status == 201) {
-        alert("회원가입이 완료되었습니다. 로그인페이지로 이동합니다")
-        window.location.replace(`${front_base_url}/templates/login.html`)
+        alert("이메일을 확인해주세요.")
     } else {
         alert("이미가입된 유저입니다.")
     }
 
     console.log(response)
+
 }
 
-// 로그인
-async function handleLogin() {
-    // const email = document.getElementById("email").value 
-    const username = document.getElementById("username").value
-    const password = document.getElementById("password").value
-    console.log(username, password)
+// 이메일 인증 재전송
+async function handleEmailValify() {
+    const email = document.getElementById("email").value
+    console.log(email)
 
-
-    const response = await fetch(`${back_base_url}/users/api/token/`, {
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/login/`, {
         headers: {
             'content-type': 'application/json',
         },
         method: 'POST',
         body: JSON.stringify({
-            // "email": email,
+            "username": username,
+            "password": password
+        })
+    })
+}
+
+// 로그인
+async function handleLogin() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+    console.log(username, password)
+
+    const response = await fetch(`${back_base_url}/users/login/`, {
+        headers: {
+            'content-type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
             "username": username,
             "password": password
         })
@@ -63,7 +110,7 @@ async function handleLogin() {
         alert("로그인되었습니다.")
         window.location.replace(`${front_base_url}/index.html`)
     } else {
-        alert("잘못된 정보입니다. 다시입력해주세요")
+        alert("이메일 인증먼저 ")
     }
 
 
@@ -92,10 +139,16 @@ async function handleMock() {    // handle=눌렀을때 실행(처리)한다는 
 
 // 로그아웃
 async function handleLogout() {
+    const response = await fetch(`${back_base_url}/users/logout/`, {
+        headers: {
+            "Authorization": 'Bearer ' + localStorage.getItem("access")
+        },
+        method: 'GET',
+    })
     alert("로그아웃!")
     localStorage.removeItem("access")
     localStorage.removeItem("refresh")
     localStorage.removeItem("payload")
     location.reload();
-    // window.location.replace(`${front_base_url}/index.html`)
+    console.log(response)
 }
