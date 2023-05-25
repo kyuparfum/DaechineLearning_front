aa94bfa7f1a91d40c241d8a4644b7001f613d53a
HYnllvo <gpalsu0812@naver.com>
2023-05-25 23:27:37 +0900

    CORS 문제 해결git add .

--- static/js/api.js
+++ static/js/api.js
@@ -2,67 +2,74 @@ window.onload = () => {
     console.log("회원가입, 로그인 api")
 }
 
-// csrfToken 가져오기
-function getCookie(name) {
-    if (!document.cookie) {
-        return null;
-    }
+let token = localStorage.getItem("access")
+// const headers = {
+//     'Access-Control-Allow-Headers': 'Authorization',
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Content-Type, Authorization',
+//     'Access-Control-Allow-Methods': '*',
+//     "Content-Type": "application/json"
+// }
 
-    const xsrfCookies = document.cookie.split(';')
-        .map(c => c.trim())
-        .filter(c => c.startsWith(name + '='));
-
-    if (xsrfCookies.length === 0) {
-        return null;
-    }
-    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
-}
-const csrfToken = getCookie('csrftoken');
-if (csrfToken != '') {
-    console.log('csrf값은:' + csrfToken);
-}
+// callback(null, {
+//     statusCode: 200,
+//     body: "Hello, world!",
+//     headers: {
+//         "access-control-allow-origin": "*",
+//         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//         'Access-Control-Allow-Methods': '*',
+//         'Access-Control-Allow-Credentials': 'True',
+//     },
+// });
 
 // 회원가입
 async function handleSignin() {
+    // try {
     const email = document.getElementById("email").value
     const username = document.getElementById("username").value
     const password1 = document.getElementById("password1").value
     const password2 = document.getElementById("password2").value
     console.log(email, username, password1, password2)
 
-    let formData = new FormData();
-    formData.append(email, email)
-    formData.append(username, username)
-    formData.append(password1, password1)
-    formData.append(password2, password2)
-    console.log(formData)
+    if (password2 !== password1) {
+        alert("비번 잘못된입력입니다. 확인해주세요.")
+        window.location.reload()
+    } else if (!email || !username || !password1 || !password2) {
+        alert("공란 잘못된입력입니다. 확인해주세요.")
+        window.location.reload()
+    }
 
-    const response = await fetch(`${back_base_url}/users/signup/`, {
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/registration/`, {
         headers: {
-
-            'content-type': 'multipart/form-data',
-            'X-CSRF-TOKEN': csrfToken,
-
+            "Content-Type": "application/json",
         },
-        // xhr.setRequestHeader('X-CSRF-Token', csrf_cookieValue);
         method: 'POST',
-        // body: JSON.stringify({
-        //     "email": email,
-        //     "username": username,
-        //     "password1": password1,
-        //     "password2": password2
-        // })
-        body: formData
+        body: JSON.stringify({
+            "email": email,
+            "username": username,
+            "password1": password1,
+            "password2": password2
+        })
+
     })
+    console.log(response)
 
     if (response.status == 201) {
         alert("이메일을 확인해주세요.")
+        window.location.reload()
     } else {
+        console.error(error.msg)
+        // alert(error)
         alert("이미가입된 유저입니다.")
     }
-
-    console.log(response)
-
+    // } catch (err) {
+    //     errorText.innerHTML = '에러메시지 : ' + err;
+    // }
+    // } catch (error) {
+    //     error.text().then(msg => alert(msg))
+    //     // alert(error.msg)
+    //     alert("이미가입된 유저입니다.")
+    // }
 }
 
 // 이메일 인증 재전송
@@ -70,16 +77,25 @@ async function handleEmailValify() {
     const email = document.getElementById("email").value
     console.log(email)
 
-    const response = await fetch(`${back_base_url}/users/dj-rest-auth/login/`, {
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/registration/resend-email/`, {
         headers: {
-            'content-type': 'application/json',
+            headers,
         },
         method: 'POST',
         body: JSON.stringify({
-            "username": username,
-            "password": password
+            "email": email,
         })
     })
+    console.log(response)
+    if (response.status == 200) {
+        alert("이메일을 확인해주세요.")
+        window.location.reload()
+    } else {
+        console.error(error.msg)
+        // alert(error)
+        alert("가입되지 않은 이메일입니다. 다시확인해주세요.")
+    }
+
 }
 
 // 로그인
@@ -88,9 +104,10 @@ async function handleLogin() {
     const password = document.getElementById("password").value
     console.log(username, password)
 
-    const response = await fetch(`${back_base_url}/users/login/`, {
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/login/`, {
         headers: {
-            'content-type': 'application/json',
+            "Content-Type": "application/json",
+
         },
         method: 'POST',
         body: JSON.stringify({
@@ -110,7 +127,7 @@ async function handleLogin() {
         alert("로그인되었습니다.")
         window.location.replace(`${front_base_url}/index.html`)
     } else {
-        alert("이메일 인증먼저 ")
+        alert("이메일 인증먼저")
     }
 
 
@@ -126,24 +143,15 @@ async function handleLogin() {
 
 }
 
-async function handleMock() {    // handle=눌렀을때 실행(처리)한다는 의미
-    const response = await fetch(`${back_base_url}/users/mock/`, {
-        headers: {
-            "Authorization": 'Bearer ' + localStorage.getItem("access")
-        },
-        method: 'GET',
-    })
-
-    console.log(response)
-}
-
 // 로그아웃
 async function handleLogout() {
-    const response = await fetch(`${back_base_url}/users/logout/`, {
+    const response = await fetch(`${back_base_url}/users/dj-rest-auth/logout/`, {
         headers: {
-            "Authorization": 'Bearer ' + localStorage.getItem("access")
+            // ${ back_base_url }
+            'Authorization': `Bearer ${token}`,
+            // 'Access-Control-Allow-Credentials': 'true'
         },
-        method: 'GET',
+        method: 'POST',
     })
     alert("로그아웃!")
     localStorage.removeItem("access")
